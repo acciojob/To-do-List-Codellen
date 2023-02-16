48d5a8a33fb9e55ef3a985e4c689a90daf24dd13
Sumeet Turkar <121540275+Codellen@users.noreply.github.com>
2023-02-16 21:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+var toDoList = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var orderList = document.getElementById('todoList');
+function addFunction(){
+	if(toDoList.value){
+	var list = document.createElement('li');
+	list.textContent=toDoList.value;
+	orderList.appendChild(list);
+	toDoList.value="";
+}
+}
 
+btn.addEventListener('click',addFunction)
